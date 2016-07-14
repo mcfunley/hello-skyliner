85e8772664f6e9a61870a587f4fd2cc11a5830f7
Coda Hale <coda.hale@gmail.com>
2016-07-13 18:04:42 -0700

    Move building the image into upload.sh.

--- upload.sh
+++ upload.sh
@@ -2,17 +2,23 @@
 set -euo pipefail # enable strict mode
 IFS=$'\n\t'
 
-if [[ (-z "${1-}") || (-z "${2-}") ]]; then
-    echo "./upload.sh <app token> <image filename>"
+TOKEN="${1-}"
+if [ -z $TOKEN ]; then
+    echo "./upload.sh <app token>"
     exit -1
 fi
 
-TOKEN=$1
-FILENAME=$2
+# build the image
+docker build -t hello-skyliner .
+
+# save the image to a TAR file
+docker save hello-skyliner -o image.tar
 
 # find the full git commit hash for the current commit
 COMMIT=$(git rev-parse HEAD)
+
 # get the upload URL
 URL=$(curl -s -X POST https://www.skyliner.io/images/docker/$TOKEN/$COMMIT)
+
 # upload to S3
-curl --progress-bar --upload-file $FILENAME $URL > /dev/null
+curl --progress-bar --upload-file image.tar $URL > /dev/null
